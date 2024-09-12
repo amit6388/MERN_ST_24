1e49ae88e343ab6b5b493fc177ee72ce236a432e
amit6388 <amitpoly2020@gmail.com>
2024-09-12 12:23:06 +0530

    cnange

--- Project/React_Folder_structure.js
+++ Project/React_Folder_structure.js
@@ -1,4 +1,30 @@
-my-react-app/
+
+/*==================================
+
+
+Please  suggest a project name for the project we are going to create.
+
+The names are below also you can suggest your own project name:-
+
+1.RecruitX
+2.WorkPlaceX 
+3.JobNavigator
+4.CareerSphere
+5.CareerSuite
+6.CareerGate
+7.CareerPath
+8.TalentHunt
+
+
+
+========================================================================= 
+
+
+
+FOLDER STRUCTURE OF OUR PROJECT 
+                            ===>
+                                
+RecruitX    #PROJECT NAME
 ├── node_modules/
 ├── public/    # yaha static file aur photos rhega jo project me use krenge hm log.
 ├── src/
@@ -19,3 +45,7 @@ my-react-app/
 ├── package.json
 ├── vite.config.js
 └── README.md
+
+
+
+*/
